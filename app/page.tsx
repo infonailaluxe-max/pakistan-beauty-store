7b11cbab5d913174e62fe02b1fb2e1923cf4990c
infonailaluxe-max <info.nailaluxe@gmail.com>
2026-06-13 11:26:59 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Pakistan Beauty Store</h1>
+
+      <p>
+        Skincare, Makeup, Hair Care and Beauty Products
+      </p>
+    </main>
+  );
+}
